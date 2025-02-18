abc5fa26e3813a846423a30ecda4b6140da3fe08
Karla G. <k.grajalesluna@gmail.com>
2025-02-18 19:39:30 +0000

    feat: add dropdown section and common.js for user rendering
    - Added the dropdown section to the user interface.
    - Included common.js to handle user rendering logic.

--- script.js
+++ script.js
@@ -1,14 +1,59 @@
-// This is a placeholder file which shows how you can access functions defined in other files.
-// It can be loaded into index.html.
-// You can delete the contents of the file once you have understood how it works.
-// Note that when running locally, in order to open a web page which uses modules, you must serve the directory over HTTP e.g. with https://www.npmjs.com/package/http-server
-// You can't open the index.html file using a file:// URL.
+import { getUserIDs, getListenEvents, getSong } from "./data.js";
 
-import { getUserIDs } from "./data.js";
+function createContent() {
+  const container = document.querySelector(".container");
+
+  const userSection = document.createElement("section");
+  userSection.id = "userSection";
+
+  const userSelector = document.createElement("select");
+
+
+  //--------------------- style in the dropdown section ------------
+  Object.assign(userSection.style, {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: "10px",
+    backgroundColor: "yellow",
+    marginBottom: "10px",
+  });
+
+  
+  userSection.appendChild(userSelector);
+  container.appendChild(userSection);
+}
+
 
 window.onload = function () {
   const users = getUserIDs();
-  document.querySelector("body").innerText = `There are ${users.length} users`;
+ 
+  createContent();
 };
 
 
+
+
+// -------------- testing what user listen in their history -----------------
+// const users = getUserIDs();
+// console.log(`There are ${users.length} users`);
+
+// const userID = "4";
+// const events = getListenEvents(userID);
+
+// events.forEach((event) => {
+//   const song = getSong(event.song_id);
+//   console.log(
+//     `the user ${userID} listen "${song.title}" of ${song.artist} on ${event.timestamp}`
+//   );
+// });
+
+
+
+// window.onload = function () {
+//   const users = getUserIDs();
+//   document.querySelector("body").innerText = `There are ${
+//     getListenEvents().length
+//   } users`;
+//   createContent();
+// };
