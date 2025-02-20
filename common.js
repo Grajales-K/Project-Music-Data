199e121892bf3b9128ed301df6ba41d78f884725
Karla G. <k.grajalesluna@gmail.com>
2025-02-20 14:26:46 +0000

    logic file

--- common.js
+++ common.js
@@ -1,3 +1,13 @@
-
-
-
+/*
+  Functions to create
+  Most listened song by count
+  Most listened song by time
+  Most listened song by count
+  Most listened song by time
+  Most listened artist by count
+  Most listened artist by time
+  Most listened song on Friday nights (by count and time)
+  Longest streak song
+  Everyday songs
+  Top genres
+*/
